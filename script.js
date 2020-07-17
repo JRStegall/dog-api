6810dabd2999d2dd8b2b90bf30d3c175b0117d70
Jeremy <jeremy.stegall77@gmail.com>
2020-07-17 11:17:12 -0400

    Dog API

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#generateRandomDogo").click(function(){
+        console.log('button clicked');
+        $.get("https://dog.ceo/api/breeds/image/random",)
+        .done(function(data) {
+            console.log(data);
+            $('#dogoContainer').html(renderDogoImage(data.message));
+        })
+        .fail(function(error){
+            console.log(error);
+        });
+    });
+    function renderDogoImage(image){
+        return `
+        <img src="${image}" class="mw-100 mh-100" />
+        `;
+    }
+});
